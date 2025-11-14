eaf2045a48547c9bbcf18bf3239f41b2ae8524b7
jayachandrakaturu <31104112+jayachandrakaturu@users.noreply.github.com>
2025-11-14 09:39:09 +0530

    component added

---
+++ component_1.ts
@@ -0,0 +1,80 @@
+import { CommonModule } from '@angular/common'
+import { ChangeDetectionStrategy, Component, DestroyRef, inject, input, OnDestroy, OnInit } from '@angular/core'
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop'
+import { FormControl, FormGroup, FormGroupDirective, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms'
+import { MatCheckboxModule } from '@angular/material/checkbox'
+import { MatIconModule } from '@angular/material/icon'
+import { MatInputModule } from '@angular/material/input'
+import { MatSelectModule } from '@angular/material/select'
+import { Observable } from 'rxjs'
+import { FaaNotamModel, PartialClosureModel } from '../../models'
+import { LookupCacheStore } from '../../store/lookup-cache-store'
+@Component({
+    standalone: true, selector: 'app-taxiway-location',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    imports: [
+        MatInputModule,
+        CommonModule,
+        FormsModule,
+        ReactiveFormsModule,
+        MatSelectModule,
+        MatIconModule,
+        MatCheckboxModule
+    ],
+    templateUrl: './taxiway-location.component.html'
+})
+export class TaxiwayLocationComponent implements OnInit, OnDestroy {
+    protected taxiwayLocationForm!: FormGroup
+    public model = input<FaaNotamModel | null>()
+    public partialClosureLocation$!: Observable<PartialClosureModel[]>
+    private form!: FormGroup
+    private destroyRef = inject(DestroyRef)
+    public constructor(
+        private readonly formGroupDirective: FormGroupDirective,
+        private readonly lookupCacheStore: LookupCacheStore
+    ) { }
+
+    public ngOnInit(): void {
+        this.form = this.formGroupDirective.form
+        this.buildForm()
+
+        this.partialClosureLocation$ = this.lookupCacheStore.partialClosureLocation$
+
+        this.lookupCacheStore.fetchPartialLocations({ keyword: this.form.value?.keyword, location: this.form.value?.location })
+
+        const taxiwayLocation = this.model()?.scenarioData.taxiwayLocation
+        this.taxiwayLocationForm.patchValue({
+            between: taxiwayLocation?.between,
+            and: taxiwayLocation?.and
+        })
+
+        this.taxiwayLocationForm.valueChanges.pipe(takeUntilDestroyed(this.destroyRef)).subscribe(res => {
+            const controls = this.taxiwayLocationForm.controls;
+            if (res.between || res.and) {
+                controls.forEach(control => {
+                    control.setValidators([Validators.required])
+                    control.updateValueAndValidity()
+                })
+            } else {
+                controls.forEach(control => {
+                    control.clearValidators()
+                    control.updateValueAndValidity()
+                })
+            }
+        })
+    }
+
+    public ngOnDestroy(): void {
+        const scenarioData = this.form.get('scenarioData') as FormGroup
+        scenarioData.removeControl('taxiwayLocation')
+    }
+
+    private buildForm(): void {
+        this.taxiwayLocationForm = new FormGroup({
+            between: new FormControl(''),
+            and: new FormControl('')
+        })
+        const scenarioData = this.form.get('scenarioData') as FormGroup
+        scenarioData.addControl('taxiwayLocation', this.taxiwayLocationForm)
+    }
+}
